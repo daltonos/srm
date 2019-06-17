01e6ebd2a7c374e26996e7cfd0099a6a47296753
C5252731 <dalton.de.oliveira.silva@sap.com>
2019-06-17 20:01:02 -0300

    finishing up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Action } from './action'
+import { MenuOption } from './menuOption'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,23 +10,44 @@ import { Action } from './action'
 export class AppComponent implements OnInit {
   title = 'srm';
   actions:Action[];
+  menuOptions:MenuOption[];
+  menuSelectedIndex:Number;
   actionSelected:Number;
+  actionSelected2:Number;
+  actionSelected3:Number;
 
   ngOnInit () {
     this.actions = [
-      {Id: 0, Label: "Action"},
-      {Id: 1, Label: "Action 1"},
-      {Id: 2, Label: "Action 2"},
-      {Id: 3, Label: "Action 3"}
+      {Id: 0, Label: "Ações"},
+      {Id: 1, Label: "Ação 1"},
+      {Id: 2, Label: "Ação 2"},
+      {Id: 3, Label: "Ação 3"}
     ];
     this.actionSelected = 0;
+    this.actionSelected2 = 0;
+    this.actionSelected3 = 0;
+
+    this.menuSelectedIndex = 0;
+    this.menuOptions = [
+      {Id: 0, Label: "Painel de Gestão", ImageSrc: "../assets/img/internetWhite.png"},
+      {Id: 1, Label: "Conta Digital", ImageSrc: "../assets/img/arroba.png"},
+      {Id: 2, Label: "Ant. de Recebíveis", ImageSrc: "../assets/img/chart.png"}
+    ];
   }
 
   onActionSelected (val:any) {
     this.logValue(val);
+    
+  }
+
+  onMenuItemClicked (val:any) {
+    this.logValue(val);
+    this.menuSelectedIndex = val;
   }
 
   logValue (val:any) {
     console.log("option '" + val + "' has been selected.");
   }
+
+
 }
---
+++ src/app/menuOption.ts
@@ -0,0 +1,5 @@
+export class MenuOption {
+    public Id: Number;
+    public Label: String;
+    public ImageSrc: String;
+}
